e829653a2376c59252c90878af092f3cf4438307
palmatemark9i <77265996+Pal-Mate-Mark-9i@users.noreply.github.com>
2025-01-21 12:27:23 +0100

    Default User pic, UserListACtivity
    Added def profil pic,
    Started UserListActivity

--- app/src/main/java/com/example/kotlin_basics/UserListActivity.kt
+++ app/src/main/java/com/example/kotlin_basics/UserListActivity.kt
@@ -8,13 +8,10 @@ import androidx.core.view.WindowInsetsCompat
 
 class UserListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_user_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val users = listOf(
+            User("Gipsz Jakab","g@mail.hu",R.drawable.user)
+        )
+
     }
 }
